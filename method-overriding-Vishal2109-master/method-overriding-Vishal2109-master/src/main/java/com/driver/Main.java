7542da0f711e1ecd10a0aee63e259c8df3237225
Vishal2109 <137199815+Vishal2109@users.noreply.github.com>
2024-01-20 10:32:09 +0530

    Add files via upload

---
+++ method-overriding-Vishal2109-master/method-overriding-Vishal2109-master/src/main/java/com/driver/Main.java
@@ -0,0 +1,12 @@
+import com.driver.A;
+import com.driver.B;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        B b = new B();
+        String ans = b.meth();
+
+        System.out.println(ans);
+    }
+}
